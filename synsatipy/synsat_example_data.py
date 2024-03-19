10b7075e8d8883f41313a8bb3f135301ccfdb6cf
Fabian Senf <senf@tropos.de>
2024-03-19 14:55:41 +0100

    added example data links for DKRZ

--- synsatipy/synsat_example_data.py
+++ synsatipy/synsat_example_data.py
@@ -18,13 +18,21 @@ def get_example_data(example_name, stored_on_server=True):
     if example_name == "era01":
 
         if server == "tropos":
-            dirname = "/vols/fs1/store/senf/data/era5/medi/2020/"
+            dirname = "/vols/fs1/store/senf/data/era5/medi/2020"
+            fname = f"{dirname}/era5-3d-medi-2020-09-15.nc"
+
+        elif server == "dkrz":
+            dirname = "/work/bb1262/data/era5/medi/2020"
             fname = f"{dirname}/era5-3d-medi-2020-09-15.nc"
 
     if example_name == "icon01":
 
         if server == "tropos":
             dirname = "/vols/fs1/store/senf/data/icon/ifces2/atlantic-cases/paulette/ifces2-atlanXL-20200907-exp021/POSTPROC/"
+            fname = f"{dirname}/3d_full_base_DOM01_ML_20200912T000000Z_regrid7km.nc"
+
+        elif server == "dkrz":
+            dirname = "/work/bb1376/data/icon/atlantic-cases/paulette/ifces2-atlanXL-20200907-exp021/POSTPROC/"
             fname = f"{dirname}/3d_full_base_DOM01_ML_20200912T000000Z_regrid7km.nc"
 
     return fname

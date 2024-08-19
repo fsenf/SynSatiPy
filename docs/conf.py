4a4ec1b8051a8820b7b286a9d6022b867173522b
Fabian Senf <senf@tropos.de>
2024-08-19 09:49:36 +0200

    added own css theme to have flexible window width

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,12 @@ myst_enable_extensions = [
 ]
 
 html_theme = "sphinx_rtd_theme"
+html_style = "css/my_theme.css"
 
-html_static_path = ["_build/html"]
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 project = "SynSatiPy"
